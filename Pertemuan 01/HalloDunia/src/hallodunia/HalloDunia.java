e6b894656ba0f79e9f9daf880a2652ed26699bc0
Yodi <94235852+fakihAlim@users.noreply.github.com>
2022-01-13 21:47:56 +0700

    tambah baris baru

--- Pertemuan 01/HalloDunia/src/hallodunia/HalloDunia.java
+++ Pertemuan 01/HalloDunia/src/hallodunia/HalloDunia.java
@@ -10,6 +10,7 @@ public class HalloDunia {
 
     public static void main(String[] args) {
        System.out.println("Hallo Dunia Satu Keluarga");
+       System.out.println("Coba lagi");
     }
 
 }
