c01a6cada960a9b0277667b44b5843f02b60bcc9
Yodi <94235852+fakihAlim@users.noreply.github.com>
2022-01-13 21:41:52 +0700

    pertemuan 1

---
+++ Pertemuan 01/HalloDunia/src/hallodunia/HalloDunia.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package hallodunia;
+
+
+public class HalloDunia {
+
+    public static void main(String[] args) {
+       System.out.println("Hallo Dunia Satu Keluarga");
+    }
+
+}
